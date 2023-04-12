0a9a7bdba329bdeabdb0346b7d2f041a855016d1
khalil <khalillakhdharatc@gmail.com>
2023-04-12 09:29:01 +0100

    Initial commit

---
+++ src/Person.ts
@@ -0,0 +1,5 @@
+export interface Personne {
+    nom: string;
+    age: number;
+    sexe: string;
+  }
---
+++ src/tableau.ts
@@ -0,0 +1,35 @@
+import { Personne } from "./Person";
+/**
+ * @constructor
+ * @param {number} nombreElements
+ * @returns {Personne[]}
+ * @description Cette fonction permet de générer un tableau de personnes aléatoires
+ */
+export function genererTableauAleatoire(nombreElements: number): Personne[] {
+  const personnes: Personne[] = [];
+  for (let i = 0; i < nombreElements; i++) {
+    const sexe = Math.random() < 0.5 ? "masculin" : "féminin";
+    const age = Math.floor(Math
+        .random() * 100) + 1;
+        const nom = sexe === "masculin" ? genererNomMasculin() : genererNomFeminin();
+        const personne: Personne = { nom, age, sexe };
+        personnes.push(personne);
+        }
+        return personnes;
+        }
+        
+        function genererNomMasculin(): string {
+        const prenoms = ["Jean", "Pierre", "Marc", "Paul", "Luc", "Thierry"];
+        const noms = ["Dubois", "Dupont", "Lefebvre", "Durand", "Martin", "Besson"];
+        const prenom = prenoms[Math.floor(Math.random() * prenoms.length)];
+        const nom = noms[Math.floor(Math.random() * noms.length)];
+        return `${prenom} ${nom}`;
+        }
+        
+        function genererNomFeminin(): string {
+        const prenoms = ["Marie", "Sophie", "Céline", "Julie", "Elodie", "Aurélie"];
+        const noms = ["Dubois", "Dupont", "Lefebvre", "Durand", "Martin", "Besson"];
+        const prenom = prenoms[Math.floor(Math.random() * prenoms.length)];
+        const nom = noms[Math.floor(Math.random() * noms.length)];
+        return `${prenom} ${nom}`;
+        }
